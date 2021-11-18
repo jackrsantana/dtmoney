6cb89d8a69bfdf86c33f2dd2d6532723ef022c3c
Jackson Santana <jack.rsantana@gmail.com>
2021-11-18 00:17:19 -0300

    Formatando valores

--- src/components/TransactionsTables/index.tsx
+++ src/components/TransactionsTables/index.tsx
@@ -34,9 +34,15 @@ export function TransactionsTables() {
           {transactions.map(transaction => (
             <tr key={transaction.type}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>transaction.amount</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
             </tr>
           ))}
         </tbody>
